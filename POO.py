eb93555cb87b9888edc064307c1de4354cd04e25
DAVIDSGERONE <davidsgerone@gmail.com>
2024-08-22 14:30:39 -0600

    modified:   POO.py

--- POO.py
+++ POO.py
@@ -1,1 +1,9 @@
-class 
+class Automovil:
+    def tipo_color(self,inColor,inRed,inBlue,inGreen) :
+        print(f"El color del auto es: {inColor}")
+        print(f"El color del auto es: {inRed}")
+        print(f"El color del auto es: {inBlue}")
+        print(f"El color del auto es: {inGreen}")
+
+        obj= Automovil()
+        obj.tipo_color(inColor="Blue",inBlue="255",inGreen="0",inRed="0")
