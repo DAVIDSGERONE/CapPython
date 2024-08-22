1f1c10fcf098aec5cd492fa3cfd1dc07bd356798
DAVIDSGERONE <davidsgerone@gmail.com>
2024-08-22 14:41:42 -0600

    Agregacion de color

--- POO.py
+++ POO.py
@@ -1,9 +1,16 @@
 class Automovil:
-    def tipo_color(self,inColor,inRed,inBlue,inGreen) :
+    def tipo_color(self, inColor, inRed, inBlue, inGreen):
         print(f"El color del auto es: {inColor}")
-        print(f"El color del auto es: {inRed}")
-        print(f"El color del auto es: {inBlue}")
-        print(f"El color del auto es: {inGreen}")
+        print(f"Valor del rojo: {inRed}")
+        print(f"Valor del azul: {inBlue}")
+        print(f"Valor del verde: {inGreen}")
 
-        obj= Automovil()
-        obj.tipo_color(inColor="Blue",inBlue="255",inGreen="0",inRed="0")
+# Crear una instancia de Automovil y llamar al método tipo_color
+
+Spark = Automovil()
+Aveo = Automovil()
+Bmw = Automovil()
+
+Spark.tipo_color(inColor="Blue", inRed="0", inBlue="255", inGreen="0")
+Aveo.tipo_color(inColor="Blue", inRed="0", inBlue="255", inGreen="0")
+Bmw.tipo_color(inColor="Blue", inRed="0", inBlue="255", inGreen="0")
