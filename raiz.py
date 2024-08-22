d71d7fbe7cb337ddc62f77dc715a2f9fbaf5bf10
DAVIDSGERONE <davidsgerone@gmail.com>
2024-08-22 15:20:57 -0600

    Cambios Clases

---
+++ raiz.py
@@ -0,0 +1,50 @@
+from Usuarios import Usuario
+
+"""
+    Seccionar todo el código (Diferentes Clases)
+"""
+# Sección de Variables 
+list_usuarios = []
+objeto_usuario = Usuario()
+
+while True:
+    print(" Plataforma de Usuarios ")
+    print(" 1.- Agrega Usuarios ")
+    print(" 2.- Eliminar Usuarios ")
+    print(" 3.- Actualizar Usuarios ")
+    print(" 4.- Salida ")
+
+    opciones_menu = input(" Introduce una opción")
+
+
+    if int(opciones_menu) == 1:
+        print(" Agregar Usuarios")
+        in_usuario = input("Introduce el nombre")
+        list_usuarios.append(objeto_usuario.mostrar_nombre(in_nombre=in_usuario))
+        print(list_usuarios)
+        
+
+    elif int(opciones_menu) == 2: 
+        print("Eliminar Usuarios")
+        print(list_usuarios)
+        try:
+            nombre = input('quien eliminas ')
+            list_usuarios.remove(nombre)
+        except Exception as usuario_no_encontrado:
+            print("Ese usuario no se encuentro")
+
+    elif int(opciones_menu) == 3:
+        print("Actualizar Usuarios")
+
+    else:
+        print("Gracias")
+
+"""
+    inp = '1'
+    match inp:
+        case '1':
+            print('Elegiste la opción 1')
+        case '2':
+            print('Elegiste la opción 2')"""
+
+    
