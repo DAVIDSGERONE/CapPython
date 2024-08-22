df144cd15af2d10ba1faa7e4bf855b0fcde7af1e
DAVIDSGERONE <davidsgerone@gmail.com>
2024-08-22 14:01:57 -0600

    modified:   Excepciones.py

--- Excepciones.py
+++ Excepciones.py
@@ -1,1 +1,9 @@
+#un ejemplo de error
+#print (1/0) 0-9
+numero_var ="hola"
+try:
+    print(int(numero_var))
+except Exception as error:
+    print(f"el valor que se recibe no es tipo entero{error}")
+    
 print("Hola mundo")
